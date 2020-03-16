d039729693ad264b1c1e95cba5e17a89e0512da2
nastya-volk <33732479+nastya-volk@users.noreply.github.com>
2020-03-16 21:12:42 +0300

    add task 2

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,23 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    int sum = 0;
+    for (int i = 0; i < [array count]; i++) {
+        sum += [array[i] intValue];
+    }
+    int subSum = sum;
+    NSMutableArray* arrayOfSubSums = [[NSMutableArray alloc] init];
+    NSMutableArray* arrayOfMinMax = [[NSMutableArray alloc] init];
+    for (int i = 0; i < [array count]; i++) {
+        subSum = subSum - [array[i] intValue];
+        [arrayOfSubSums addObject:@(subSum)];
+        subSum = subSum + [array[i] intValue];
+    }
+    NSNumber *max=[arrayOfSubSums valueForKeyPath:@"@max.self"];
+    NSNumber *min=[arrayOfSubSums valueForKeyPath:@"@min.self"];
+    [arrayOfMinMax addObject:min];
+    [arrayOfMinMax addObject:max];
+    return arrayOfMinMax;
 }
 
 @end
