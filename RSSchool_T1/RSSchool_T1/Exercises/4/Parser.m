386f1c2e8c516191e9dac95eb0adfe6345023bc6
nastya-volk <33732479+nastya-volk@users.noreply.github.com>
2020-03-16 21:14:36 +0300

    add task 4

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,70 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    NSMutableArray* parsedArray = [[NSMutableArray alloc] init];
+    NSString *stringForArray;
+    int min = 0;
+    int max = 0;
+    int maxForRange = 0;
+    int count = 0;
+    for (int i = min; i < string.length; i++) {
+        if ([string characterAtIndex:i] == '<' || [string characterAtIndex:i] == '(' || [string characterAtIndex:i] == '[') {
+            
+            if ([string characterAtIndex:i] == '<') {
+                i += 1;
+                min = i;
+                count++;
+                    while ([string characterAtIndex:i] != '>') {
+                        if ([string characterAtIndex:i] == '<') {
+                            i += 1;
+                            count++;
+                            max++;
+                            while (count != 0) {
+                                    max++;
+                                    i++;
+                              
+                                if ([string characterAtIndex:i] == '>') {
+                                    count--;
+                                   
+                                }
+                            }
+                        }
+                        else {
+                        max++;
+                        i++;
+                        }
+                    }
+            }
+            
+            
+            
+            if ([string characterAtIndex:i] == '(') {
+                i += 1;
+                min = i;
+                while ([string characterAtIndex:i] != ')') {
+                    max++;
+                    i++;
+                }
+            }
+            if ([string characterAtIndex:i] == '[') {
+                i += 1;
+                min = i;
+                while ([string characterAtIndex:i] != ']') {
+                    max++;
+                    i++;
+                }
+            }
+            i = min - 1;
+            maxForRange = min + max - 1;
+            stringForArray = [string substringWithRange:NSMakeRange(min, max)];
+            max = 0;
+            maxForRange = 0;
+            [parsedArray addObject:stringForArray];
+        }
+        
+    }
+    NSLog(@"NSArray ********************************************array = \n%@", parsedArray);
+    return parsedArray;
 }
 
 @end
