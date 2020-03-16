e2da606b057dc8c4b79bca383773421be46b222c
nastya-volk <33732479+nastya-volk@users.noreply.github.com>
2020-03-16 21:13:12 +0300

    add task 3

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,31 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    NSMutableArray* arrayAnnasBill = [[NSMutableArray alloc] init];
+    [arrayAnnasBill addObjectsFromArray:bill];
+    //int brianDue = 0;
+    int annDue = 0;
+    int allDue = 0;
+    int refund = 0;
+    [arrayAnnasBill removeObjectAtIndex:index];
+    for (int i = 0; i < [arrayAnnasBill count]; i++) {
+        annDue += [arrayAnnasBill[i] intValue];
+    }
+    
+    annDue = annDue / 2;
+    //brianDue = allDue - annDue;
+    int intSum = [sum intValue];
+    if (annDue == intSum) {
+        return @"Bon Appetit";
+    }
+    else {
+        for (int i = 0; i < [bill count]; i++) {
+            allDue += [bill[i] intValue];
+        }
+        refund = intSum - annDue;
+        NSString* refundString = [NSString stringWithFormat:@"%i", refund];
+        return refundString;
+    }
 }
 
 @end
